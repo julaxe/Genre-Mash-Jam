1a80df58300e4a39b738d0df913a573fc96f9f5b
david <eorjs2020@gmail.com>
2021-05-22 12:20:17 -0400

    Camera Switching

--- Robot-Platformer-TDefense/Assets/Scripts/playerMovement.cs
+++ Robot-Platformer-TDefense/Assets/Scripts/playerMovement.cs
@@ -7,20 +7,43 @@ public class playerMovement : MonoBehaviour
     [SerializeField] private float speed = 5;
     [SerializeField] private float jumpForce = 10;
     private Rigidbody2D body;
+    public Camera cam1;
+    public Camera cam2;
     // Start is called before the first frame update
     void Start()
     {
+        cam1.enabled = true;
+        cam2.enabled = false;
         body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //player movement on x axis 
-        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+        
         //player jumping
         if(Input.GetKeyDown(KeyCode.Space)) {
             body.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
         }
+        if(Input.GetKeyUp(KeyCode.G))
+        {
+            if (cam1.enabled == true)
+            {
+                cam1.enabled = false;
+                cam2.enabled = true;
+            }
+            else
+            {
+                cam1.enabled = true;
+                cam2.enabled = false;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        //player movement on x axis 
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
     }
 }
+
+
