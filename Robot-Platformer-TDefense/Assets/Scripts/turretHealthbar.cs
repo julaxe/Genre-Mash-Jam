c64995611fdcc70a9f33175089decbe22f741e1a
david <eorjs2020@gmail.com>
2021-05-26 17:12:59 -0400

    Fixed 4

--- Robot-Platformer-TDefense/Assets/Scripts/turretHealthbar.cs
+++ Robot-Platformer-TDefense/Assets/Scripts/turretHealthbar.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class turrethealthbar : MonoBehaviour
+public class turretHealthbar : MonoBehaviour
 {
     [SerializeField] private float amountOfHealth = 100;
     [SerializeField] private GameObject healthBar;
